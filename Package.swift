6fe0a23b03f40b7c4de34062fb6a3435d2849c9c
Andre Carrera <acarrera94@gmail.com>
2019-06-06 16:13:10 -0600

    added package config

--- Package.swift
+++ Package.swift
@@ -3,4 +3,10 @@ import PackageDescription
 let package = Package(
     name: "Disk",
     exclude: ["DiskExample", "Tests"]
+    products: [
+        .library(name: "Disk", targets: ["Disk"])
+    ],
+    targets: [
+        .target(name: "Disk", path: "Lib/Sources")
+    ]
 )
